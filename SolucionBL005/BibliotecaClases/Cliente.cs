ea822e78425c5670ea5662a298258da6f16fde0f
Freddy <fre.camposo@profesor.duoc.cl>
2020-04-17 15:47:52 -0400

    primera subida

---
+++ SolucionBL005/BibliotecaClases/Cliente.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+    public class Cliente
+    {
+        private string _rut;
+        private string _nombres;
+        private string _apellidos;
+        private DateTime _fechaNacimiento;
+        public int IdSexo { get; set; }
+        public int IdEstadoCivil { get; set; }
+
+        public DateTime FechaNacimiento
+        {
+            get { return _fechaNacimiento; }
+            set {
+                _fechaNacimiento = value;
+            }
+        }
+        public string Apellidos
+        {
+            get { return _apellidos; }
+            set {
+                _apellidos = value;
+            }
+        }
+        public string Nombres
+        {
+            get { return _nombres; }
+            set {
+                _nombres = value;
+            }
+        }
+        public string Rut
+        {
+            get { return _rut; }
+            set {
+                _rut = value;
+            }
+        }
+
+        public Cliente()
+        {
+
+        }
+    }
+}
---
+++ SolucionBL005/BibliotecaClases/Contrato.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaClases
+{
+    public class Contrato
+    {
+        public string Numero { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        private string _rut;
+        public string CodigoPlan { get; set; }
+        private DateTime _fechaIncioVigencia;
+        public DateTime FechaFinVigencia { get; set; }
+        public bool Vigente { get; set; }
+        public bool DeclaracionSalud { get; set; }
+        public float PrimaAnual { get; set; }
+        public float PrimaMensual { get; set; }
+        private string _observaciones;
+
+        public string Observaciones
+        {
+            get { return _observaciones; }
+            set {
+                if (value.Trim().Length>0)
+                {
+                    _observaciones = value;
+                }
+                else
+                {
+                    throw new Exception("ingrese obs");
+                }
+                
+            }
+        }
+        public DateTime FechaInicioVigencia
+        {
+            get { return _fechaIncioVigencia; }
+            set {
+                DateTime fi = DateTime.Now.Date;
+                DateTime ft = DateTime.Now.AddDays(30).Date;
+                if (value>=fi && value<=ft)
+                {
+                    _fechaIncioVigencia = value;
+                }
+                else
+                {
+                    throw new Exception("fecha incorrecta");
+                }
+                
+            }
+        }
+        public string Rut
+        {
+            get { return _rut; }
+            set {
+                _rut = value;
+            }
+        }
+
+        public Contrato()
+        {
+
+        }
+    }
+}
